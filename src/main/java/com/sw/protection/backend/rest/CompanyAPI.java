3912ae988b53d789e00c1a46fc8752cf953eca29
Dinuka <dinuka.malalanayake@gmail.com>
2014-04-17 15:42:45 -0500

    Complete CompanyAPI PUT, DELETE operations and relevant test cases

--- src/main/java/com/sw/protection/backend/rest/CompanyAPI.java
+++ src/main/java/com/sw/protection/backend/rest/CompanyAPI.java
@@ -1,8 +1,10 @@
 package com.sw.protection.backend.rest;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -16,11 +18,11 @@ import com.sw.protection.backend.common.exception.DecodingException;
 import com.sw.protection.backend.common.exception.DuplicateRecordException;
 import com.sw.protection.backend.common.exception.EncodingException;
 import com.sw.protection.backend.common.exception.OperationRollBackException;
+import com.sw.protection.backend.common.exception.RecordAlreadyModifiedException;
 import com.sw.protection.backend.common.exception.RequiredDataNotFoundException;
 import com.sw.protection.backend.config.APINames;
 import com.sw.protection.backend.config.AppContext;
 import com.sw.protection.backend.config.EncoderDecoderType;
-import com.sw.protection.backend.service.AdminService;
 import com.sw.protection.backend.service.CompanyService;
 import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiImplicitParam;
@@ -54,7 +56,7 @@ public class CompanyAPI {
 	    @ApiResponse(code = 500, message = "Internal server error due to encoding the data"),
 	    @ApiResponse(code = 400, message = "Bad request due to decoding the data"),
 	    @ApiResponse(code = 412, message = "Pre condition failed due to required data not found") })
-    public Response getAdmin(
+    public Response getCompany(
 	    @ApiParam(value = "user_name of company", required = true) @PathParam("userName") String userName) {
 	CompanyService companyService = AppContext.getInstance().getBean(CompanyService.class);
 
@@ -91,7 +93,7 @@ public class CompanyAPI {
 	    @ApiResponse(code = 500, message = "Internal server error due to encoding the data"),
 	    @ApiResponse(code = 400, message = "Bad request due to decoding the data"),
 	    @ApiResponse(code = 412, message = "Pre condition failed due to {item_per_list} or {page_number} is not grater than zero") })
-    public Response getAdminList(
+    public Response getCompanyList(
 	    @ApiParam(value = "Max Company to be featched", required = true) @PathParam("item_per_list") int itemPerList,
 	    @ApiParam(value = "Page number of company list", required = true) @PathParam("page_number") int pageNumber) {
 	CompanyService companyService = AppContext.getInstance().getBean(CompanyService.class);
@@ -130,7 +132,7 @@ public class CompanyAPI {
 	    @ApiResponse(code = 409, message = "Duplicate recode"),
 	    @ApiResponse(code = 304, message = "Not modified due to operation rollback") })
     @ApiImplicitParams({ @ApiImplicitParam(required = true, dataType = "Company", paramType = "body", allowableValues = MediaType.APPLICATION_JSON) })
-    public Response saveAdmin(String body) {
+    public Response saveCompany(String body) {
 
 	CompanyService companyService = AppContext.getInstance().getBean(CompanyService.class);
 
@@ -157,6 +159,91 @@ public class CompanyAPI {
 	    return Response.status(Status.CONFLICT).build();
 	} catch (OperationRollBackException e) {
 	    return Response.status(Status.NOT_MODIFIED).build();
+	}
+    }
+
+    @PUT
+    @Produces({ MediaType.APPLICATION_JSON })
+    @Consumes({ MediaType.APPLICATION_JSON })
+    @ApiOperation(value = "Update specific company", httpMethod = "PUT", notes = "update existing company", response = Response.class)
+    @ApiResponses(value = { @ApiResponse(code = 200, message = "Company successfuly updated"),
+	    @ApiResponse(code = 404, message = "Given company not updated"),
+	    @ApiResponse(code = 500, message = "Internal server error due to encoding the data"),
+	    @ApiResponse(code = 400, message = "Bad request due to decoding the data"),
+	    @ApiResponse(code = 412, message = "Pre condition failed due to required data not found"),
+	    @ApiResponse(code = 304, message = "Not modified due to operation rollback"),
+	    @ApiResponse(code = 206, message = "Partial content due to given recode is not the latest modification") })
+    @ApiImplicitParams({ @ApiImplicitParam(required = true, dataType = "Company", paramType = "body", allowableValues = MediaType.APPLICATION_JSON) })
+    public Response updateCompany(String body) {
+
+	CompanyService companyService = AppContext.getInstance().getBean(CompanyService.class);
+
+	try {
+	    String companyData = "";
+	    // process the JSON type request
+	    if (headers.getRequestHeaders().get(ACCEPT_HEADERS).contains(MediaType.APPLICATION_JSON)) {
+		companyData = companyService.updateCompany(EncoderDecoderType.JSON, body);
+	    }
+	    // TODO: Need to process the XML type requests
+
+	    if (companyData != "") {
+		return Response.ok().entity(companyData).build();
+	    } else {
+		return Response.status(404).build();
+	    }
+	} catch (EncodingException e) {
+	    return Response.status(Status.INTERNAL_SERVER_ERROR).build();
+	} catch (DecodingException e) {
+	    return Response.status(Status.BAD_REQUEST).build();
+	} catch (RequiredDataNotFoundException e) {
+	    return Response.status(Status.PRECONDITION_FAILED).build();
+	} catch (OperationRollBackException e) {
+	    return Response.status(Status.NOT_MODIFIED).build();
+	} catch (RecordAlreadyModifiedException e) {
+	    return Response.status(206).build();
+	}
+    }
+
+    @DELETE
+    @Path("/" + APINames.COMPANY_DELETE)
+    @Produces({ MediaType.APPLICATION_JSON })
+    @Consumes({ MediaType.APPLICATION_JSON })
+    @ApiOperation(value = "Delete specific company", httpMethod = "DELETE", notes = "delete existing company", response = Response.class)
+    @ApiResponses(value = { @ApiResponse(code = 200, message = "Company successfuly deleted"),
+	    @ApiResponse(code = 404, message = "Given company not deleted"),
+	    @ApiResponse(code = 500, message = "Internal server error due to encoding the data"),
+	    @ApiResponse(code = 400, message = "Bad request due to decoding the data"),
+	    @ApiResponse(code = 412, message = "Pre condition failed due to required data not found"),
+	    @ApiResponse(code = 304, message = "Not deleted due to operation rollback"),
+	    @ApiResponse(code = 206, message = "Partial content due to given recode is not the latest modification") })
+    @ApiImplicitParams({ @ApiImplicitParam(required = true, dataType = "Company", paramType = "body", allowableValues = MediaType.APPLICATION_JSON) })
+    public Response deleteCompany(String body) {
+
+	CompanyService companyService = AppContext.getInstance().getBean(CompanyService.class);
+
+	try {
+	    String companyData = "";
+	    // process the JSON type request
+	    if (headers.getRequestHeaders().get(ACCEPT_HEADERS).contains(MediaType.APPLICATION_JSON)) {
+		companyData = companyService.deleteCompany(EncoderDecoderType.JSON, body);
+	    }
+	    // TODO: Need to process the XML type requests
+
+	    if (companyData != "") {
+		return Response.ok().entity(companyData).build();
+	    } else {
+		return Response.status(404).build();
+	    }
+	} catch (EncodingException e) {
+	    return Response.status(Status.INTERNAL_SERVER_ERROR).build();
+	} catch (DecodingException e) {
+	    return Response.status(Status.BAD_REQUEST).build();
+	} catch (RequiredDataNotFoundException e) {
+	    return Response.status(Status.PRECONDITION_FAILED).build();
+	} catch (OperationRollBackException e) {
+	    return Response.status(Status.NOT_MODIFIED).build();
+	} catch (RecordAlreadyModifiedException e) {
+	    return Response.status(206).build();
 	}
     }
 }
