bdeb8e1df72778a7d5de00e0eee644fd1ecafa27
Dinuka <dinuka.malalanayake@gmail.com>
2014-05-01 14:47:11 -0500

    Complete CompanyScope Service operations

--- src/main/java/com/sw/protection/backend/service/impl/CompanyUserScopeServiceImpl.java
+++ src/main/java/com/sw/protection/backend/service/impl/CompanyUserScopeServiceImpl.java
@@ -1,5 +1,7 @@
 package com.sw.protection.backend.service.impl;
 
+import java.util.List;
+
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +11,7 @@ import com.sw.protection.backend.common.exception.EncodingException;
 import com.sw.protection.backend.common.exception.OperationRollBackException;
 import com.sw.protection.backend.common.exception.RecordAlreadyModifiedException;
 import com.sw.protection.backend.common.exception.RequiredDataNotFoundException;
+import com.sw.protection.backend.config.APIOperations;
 import com.sw.protection.backend.config.AppContext;
 import com.sw.protection.backend.config.EncoderDecoderType;
 import com.sw.protection.backend.config.ObjectType;
@@ -66,38 +69,159 @@ public class CompanyUserScopeServiceImpl implements CompanyUserScopeService {
     @Override
     public String getCompanyUserScope(EncoderDecoderType encoderDecoderType, String userName, String api_name)
 	    throws EncodingException, DecodingException, RequiredDataNotFoundException {
-	// TODO Auto-generated method stub
-	return null;
+	EncoderFactory encoderFactory = AppContext.getInstance().getBean(EncoderFactory.class);
+	Encoder encoder = encoderFactory.getEncoder(encoderDecoderType);
+	CompanyUserScopeDAO companyUserScopeDAO = AppContext.getInstance().getBean(CompanyUserScopeDAO.class);
+	String encodedString = "";
+	if (userName != "" && api_name != "") {
+	    CompanyUserScope companyUserScope = companyUserScopeDAO.getCompanyUserScope(userName, api_name);
+	    if (companyUserScope != null) {
+		encodedString = encoder.encodeObject(ObjectType.COMPANY_USER_SCOPE, companyUserScope);
+	    } else {
+		if (log.isDebugEnabled()) {
+		    log.debug("Company User scopes is empty so return empty string as encoded user scope string");
+		}
+	    }
+	} else {
+	    if (log.isDebugEnabled()) {
+		log.debug("User name or Api name is empty string");
+	    }
+	    throw new RequiredDataNotFoundException("User name or Api name is empty");
+	}
+
+	return encodedString;
     }
 
     @Override
     public String getCompanyUserScopes(EncoderDecoderType encoderDecoderType, String userName)
 	    throws EncodingException, DecodingException, RequiredDataNotFoundException {
-	// TODO Auto-generated method stub
-	return null;
+	EncoderFactory encoderFactory = AppContext.getInstance().getBean(EncoderFactory.class);
+	Encoder encoder = encoderFactory.getEncoder(encoderDecoderType);
+	CompanyUserScopeDAO companyUserScopeDAO = AppContext.getInstance().getBean(CompanyUserScopeDAO.class);
+	String encodedString = "";
+	if (userName != "") {
+	    List<CompanyUserScope> companyUserScopes = companyUserScopeDAO.getAllCompanyUserScopes(userName);
+	    if (companyUserScopes != null) {
+		encodedString = encoder.encodeObjectList(ObjectType.COMPANY_USER_SCOPE, companyUserScopes);
+	    } else {
+		if (log.isDebugEnabled()) {
+		    log.debug("Company user scopes list is empty so return empty string as encoded user scope list string");
+		}
+	    }
+	} else {
+	    if (log.isDebugEnabled()) {
+		log.debug("User name is empty string");
+	    }
+	    throw new RequiredDataNotFoundException("Username is empty");
+	}
+
+	return encodedString;
     }
 
     @Override
     public String isAccessGrantedFor(String userName, String api_name, String api_opertaion)
 	    throws RequiredDataNotFoundException {
-	// TODO Auto-generated method stub
-	return null;
+	CompanyUserScopeDAO companyUserScopeDAO = AppContext.getInstance().getBean(CompanyUserScopeDAO.class);
+	String encodedString = "";
+	if (userName != "" && api_name != "" && api_opertaion != "") {
+	    boolean apiOptEnumExist = false;
+	    for (APIOperations a : APIOperations.values()) {
+		if (a.toString().equals(api_opertaion)) {
+		    apiOptEnumExist = true;
+		    break;
+		}
+	    }
+
+	    // check the api operation is match with the enum
+	    if (apiOptEnumExist) {
+		boolean result = companyUserScopeDAO.isAccessGrantedFor(userName, api_name,
+			APIOperations.valueOf(api_opertaion));
+		encodedString = String.valueOf(result);
+	    } else {
+		if (log.isDebugEnabled()) {
+		    log.debug("Api operation is invalid:" + api_opertaion);
+		}
+		throw new RequiredDataNotFoundException("Api operation is invalid :" + api_opertaion);
+	    }
+
+	} else {
+	    if (log.isDebugEnabled()) {
+		log.debug("User name, Api name or Api operation is empty string");
+	    }
+	    throw new RequiredDataNotFoundException("User name, Api name or Api operation is empty string");
+	}
+
+	return encodedString;
     }
 
     @Override
     public String deleteCompanyUserScope(EncoderDecoderType encoderDecoderType, String companyUserScopeString)
 	    throws RecordAlreadyModifiedException, OperationRollBackException, EncodingException, DecodingException,
 	    RequiredDataNotFoundException {
-	// TODO Auto-generated method stub
-	return null;
+	EncoderFactory encoderFactory = AppContext.getInstance().getBean(EncoderFactory.class);
+	DecoderFactory decoderFactory = AppContext.getInstance().getBean(DecoderFactory.class);
+	Encoder encoder = encoderFactory.getEncoder(encoderDecoderType);
+	Decoder decoder = decoderFactory.getDecoder(encoderDecoderType);
+	CompanyUserScopeDAO companyUserScopeDAO = AppContext.getInstance().getBean(CompanyUserScopeDAO.class);
+	String encodedString = "";
+
+	CompanyUserScope companyUserScope = (CompanyUserScope) decoder.decodeObject(ObjectType.COMPANY_USER_SCOPE,
+		companyUserScopeString);
+	if (companyUserScope != null) {
+	    if (companyUserScopeDAO.validateCompanyUserScopeforUpdateandDelete(companyUserScope)) {
+		companyUserScope = companyUserScopeDAO.deleteCompanyUserScope(companyUserScope);
+		encodedString = encoder.encodeObject(ObjectType.COMPANY_USER_SCOPE, companyUserScope);
+	    } else {
+		if (log.isDebugEnabled()) {
+		    log.debug("Given CompanyUserScope object doesn't contain expected data:");
+		}
+		throw new RequiredDataNotFoundException(
+			"Cannot delete the CompanyUserScope - Given CompanyUserScope object doesn't contain expected data");
+	    }
+	} else {
+	    if (log.isDebugEnabled()) {
+		log.debug("Given CompanyUserScope object is null");
+	    }
+	    throw new RequiredDataNotFoundException(
+		    "Cannot delete the CompanyUserScope - Given CompanyUserScope object is null");
+	}
+
+	return encodedString;
     }
 
     @Override
     public String updateCompanyUserScope(EncoderDecoderType encoderDecoderType, String companyUserScopeString)
 	    throws RecordAlreadyModifiedException, OperationRollBackException, EncodingException, DecodingException,
 	    RequiredDataNotFoundException {
-	// TODO Auto-generated method stub
-	return null;
+	EncoderFactory encoderFactory = AppContext.getInstance().getBean(EncoderFactory.class);
+	DecoderFactory decoderFactory = AppContext.getInstance().getBean(DecoderFactory.class);
+	Encoder encoder = encoderFactory.getEncoder(encoderDecoderType);
+	Decoder decoder = decoderFactory.getDecoder(encoderDecoderType);
+	CompanyUserScopeDAO companyUserScopeDAO = AppContext.getInstance().getBean(CompanyUserScopeDAO.class);
+	String encodedString = "";
+
+	CompanyUserScope companyUserScope = (CompanyUserScope) decoder.decodeObject(ObjectType.COMPANY_USER_SCOPE,
+		companyUserScopeString);
+	if (companyUserScope != null) {
+	    if (companyUserScopeDAO.validateCompanyUserScopeforUpdateandDelete(companyUserScope)) {
+		companyUserScope = companyUserScopeDAO.updateCompanyUserScope(companyUserScope);
+		encodedString = encoder.encodeObject(ObjectType.COMPANY_USER_SCOPE, companyUserScope);
+	    } else {
+		if (log.isDebugEnabled()) {
+		    log.debug("Given CompanyUserScope object doesn't contain expected data:");
+		}
+		throw new RequiredDataNotFoundException(
+			"Cannot update the CompanyUserScope - Given CompanyUserScope object doesn't contain expected data");
+	    }
+	} else {
+	    if (log.isDebugEnabled()) {
+		log.debug("Given CompanyUserScope object is null");
+	    }
+	    throw new RequiredDataNotFoundException(
+		    "Cannot update the CompanyUserScope - Given CompanyUserScope object is null");
+	}
+
+	return encodedString;
     }
 
 }
--- src/test/java/com/sw/protection/backend/service/impl/CompanyUserScopeServiceImplTest.java
+++ src/test/java/com/sw/protection/backend/service/impl/CompanyUserScopeServiceImplTest.java
@@ -11,6 +11,7 @@ import org.testng.annotations.Test;
 import com.sw.protection.backend.common.exception.DecodingException;
 import com.sw.protection.backend.common.exception.DuplicateRecordException;
 import com.sw.protection.backend.common.exception.RequiredDataNotFoundException;
+import com.sw.protection.backend.config.APIOperations;
 import com.sw.protection.backend.config.AppContext;
 import com.sw.protection.backend.config.EncoderDecoderType;
 import com.sw.protection.backend.config.ObjectType;
@@ -18,6 +19,7 @@ import com.sw.protection.backend.config.SharedInMemoryData;
 import com.sw.protection.backend.decoder.impl.JSONDecoder;
 import com.sw.protection.backend.encoder.impl.JSONEncoder;
 import com.sw.protection.backend.entity.Company;
+import com.sw.protection.backend.entity.CompanyUserScope;
 import com.sw.protection.backend.service.CompanyService;
 import com.sw.protection.backend.service.CompanyUserScopeService;
 import com.sw.protection.backend.service.CompanyUserService;
@@ -122,26 +124,152 @@ public class CompanyUserScopeServiceImplTest {
 
     @Test(dependsOnMethods = { "saveCompanyUserScope" })
     public void getCompanyUserScope() {
+	CompanyUserScopeService companyUserScopeService = AppContext.getInstance().getBean(
+		CompanyUserScopeService.class);
+	String companyUserScopeReturn = "";
+	JSONDecoder decoder = new JSONDecoder();
+	JSONEncoder encoder = new JSONEncoder();
+	try {
+	    String companyUserScopeEncoded = companyUserScopeService.getCompanyUserScope(EncoderDecoderType.JSON,
+		    "dinuka", "company-user");
+	    assertEquals(companyUserScopeEncoded.contains("\"get\":true"), true);
+	    assertEquals(companyUserScopeEncoded.contains("\"post\":true"), true);
+	    assertEquals(companyUserScopeEncoded.contains("\"put\":true"), true);
+	    assertEquals(companyUserScopeEncoded.contains("\"del\":true"), true);
 
+	    CompanyUserScope decodedCompanyUserScope = (CompanyUserScope) decoder.decodeObject(
+		    ObjectType.COMPANY_USER_SCOPE, companyUserScopeEncoded);
+	    decodedCompanyUserScope.setDel(false);
+	    decodedCompanyUserScope.setPost(false);
+	    decodedCompanyUserScope.setPut(false);
+	    decodedCompanyUserScope.setGet(false);
+	    String companyUserScopeFinalEncoded = encoder.encodeObject(ObjectType.COMPANY_USER_SCOPE,
+		    decodedCompanyUserScope);
+	    companyUserScopeReturn = companyUserScopeService.updateCompanyUserScope(EncoderDecoderType.JSON,
+		    companyUserScopeFinalEncoded);
+	} catch (Exception e) {
+	    e.printStackTrace();
+	}
+
+	assertEquals(companyUserScopeReturn.contains("id"), true);
+	assertEquals(companyUserScopeReturn.contains("\"get\":true"), false);
+	assertEquals(companyUserScopeReturn.contains("\"post\":true"), false);
+	assertEquals(companyUserScopeReturn.contains("\"put\":true"), false);
+	assertEquals(companyUserScopeReturn.contains("\"del\":true"), false);
     }
 
     @Test(dependsOnMethods = { "getCompanyUserScope" })
     public void getCompanyUserScopes() {
+	CompanyUserScopeService companyUserScopeService = AppContext.getInstance().getBean(
+		CompanyUserScopeService.class);
+	String adminScopeReturn = "";
+	try {
+	    adminScopeReturn = companyUserScopeService.getCompanyUserScopes(EncoderDecoderType.JSON, "dinuka");
+	} catch (Exception e) {
+	    e.printStackTrace();
+	}
 
+	assertEquals(adminScopeReturn.contains("\"api_name\":\"company-user\""), true);
+	assertEquals(adminScopeReturn.contains("\"api_name\":\"software\""), true);
+
+	String requiredDataNotfoundExcep = "";
+	try {
+	    adminScopeReturn = companyUserScopeService.getCompanyUserScopes(EncoderDecoderType.JSON, "");
+	} catch (Exception e) {
+	    requiredDataNotfoundExcep = e.getClass().toString();
+	}
+	assertEquals(requiredDataNotfoundExcep, RequiredDataNotFoundException.class.toString());
     }
 
     @Test(dependsOnMethods = { "getCompanyUserScopes" })
     public void isAccessGrantedFor() {
+	CompanyUserScopeService companyUserScopeService = AppContext.getInstance().getBean(
+		CompanyUserScopeService.class);
+	String companyUserScopeReturn = "";
+	try {
+	    companyUserScopeReturn = companyUserScopeService.isAccessGrantedFor("dinuka", "company-user",
+		    APIOperations.GET.toString());
 
+	} catch (Exception e) {
+	    e.printStackTrace();
+	}
+	assertEquals(companyUserScopeReturn, "false");
+
+	try {
+	    companyUserScopeReturn = companyUserScopeService.isAccessGrantedFor("dinuka", "software",
+		    APIOperations.GET.toString());
+
+	} catch (Exception e) {
+	    e.printStackTrace();
+	}
+	assertEquals(companyUserScopeReturn, "true");
     }
 
     @Test(dependsOnMethods = { "isAccessGrantedFor" })
     public void updateCompanyUserScope() {
+	CompanyUserScopeService companyUserScopeService = AppContext.getInstance().getBean(
+		CompanyUserScopeService.class);
+	String companyUserScopeReturn = "";
+	JSONDecoder decoder = new JSONDecoder();
+	JSONEncoder encoder = new JSONEncoder();
+	try {
+	    String companyUserScopeEncoded = companyUserScopeService.getCompanyUserScope(EncoderDecoderType.JSON,
+		    "dinuka", "software");
+	    assertEquals(companyUserScopeEncoded.contains("\"get\":true"), true);
+	    assertEquals(companyUserScopeEncoded.contains("\"post\":true"), true);
+	    assertEquals(companyUserScopeEncoded.contains("\"put\":true"), true);
+	    assertEquals(companyUserScopeEncoded.contains("\"del\":true"), true);
 
+	    CompanyUserScope decodedAdminScope = (CompanyUserScope) decoder.decodeObject(ObjectType.COMPANY_USER_SCOPE,
+		    companyUserScopeEncoded);
+	    decodedAdminScope.setDel(false);
+	    decodedAdminScope.setPost(false);
+	    decodedAdminScope.setPut(false);
+	    decodedAdminScope.setGet(false);
+	    String companyUserScopeFinalEncoded = encoder
+		    .encodeObject(ObjectType.COMPANY_USER_SCOPE, decodedAdminScope);
+	    companyUserScopeReturn = companyUserScopeService.updateCompanyUserScope(EncoderDecoderType.JSON,
+		    companyUserScopeFinalEncoded);
+	} catch (Exception e) {
+	    e.printStackTrace();
+	}
+
+	assertEquals(companyUserScopeReturn.contains("id"), true);
+	assertEquals(companyUserScopeReturn.contains("\"get\":true"), false);
+	assertEquals(companyUserScopeReturn.contains("\"post\":true"), false);
+	assertEquals(companyUserScopeReturn.contains("\"put\":true"), false);
+	assertEquals(companyUserScopeReturn.contains("\"del\":true"), false);
     }
 
     @Test(dependsOnMethods = { "updateCompanyUserScope" })
     public void deleteCompanyUserScope() {
+	CompanyUserScopeService companyUserScopeService = AppContext.getInstance().getBean(
+		CompanyUserScopeService.class);
+	String companyUserScopeReturn = "";
+	JSONDecoder decoder = new JSONDecoder();
+	JSONEncoder encoder = new JSONEncoder();
+	try {
+	    String companyUserScopeEncoded = companyUserScopeService.getCompanyUserScope(EncoderDecoderType.JSON,
+		    "dinuka", "software");
+	    assertEquals(companyUserScopeEncoded.contains("id"), true);
+	    assertEquals(companyUserScopeEncoded.contains("\"get\":true"), false);
+	    assertEquals(companyUserScopeEncoded.contains("\"post\":true"), false);
+	    assertEquals(companyUserScopeEncoded.contains("\"put\":true"), false);
+	    assertEquals(companyUserScopeEncoded.contains("\"del\":true"), false);
+
+	    CompanyUserScope decodedAdminScope = (CompanyUserScope) decoder.decodeObject(ObjectType.COMPANY_USER_SCOPE,
+		    companyUserScopeEncoded);
+
+	    String companyUserScopeFinalEncoded = encoder
+		    .encodeObject(ObjectType.COMPANY_USER_SCOPE, decodedAdminScope);
+	    companyUserScopeReturn = companyUserScopeService.deleteCompanyUserScope(EncoderDecoderType.JSON,
+		    companyUserScopeFinalEncoded);
+	    companyUserScopeEncoded = companyUserScopeService.getCompanyUserScope(EncoderDecoderType.JSON, "dinuka",
+		    "software");
+	    assertEquals(companyUserScopeEncoded, "");
+	} catch (Exception e) {
+	    e.printStackTrace();
+	}
 
     }
 }
